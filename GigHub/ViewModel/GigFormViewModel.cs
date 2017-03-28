704bcee1650c3fa378c915e9d1ebf63b6f98ca23
sumit753 <sumit753@gmail.com>
2017-03-29 01:39:51 +0530

    Implemented Custom Validation

--- GigHub/ViewModel/GigFormViewModel.cs
+++ GigHub/ViewModel/GigFormViewModel.cs
@@ -11,15 +11,18 @@ namespace GigHub.ViewModel
         public string Venue { get; set; }
 
         [Required]
+        [FutureDate]
         public string Date { get; set; }
 
         [Required]
+        [ValidTime]
         public string Time { get; set; }
 
         [Required]
         public byte Genre { get; set; }
 
         public IEnumerable<Genre> Genres { get; set; }
+
 
         public DateTime getDateTime()
         {
