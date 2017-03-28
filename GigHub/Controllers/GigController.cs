043a7ea36c41a95497e8f76f6d757a4a301fdbe9
sumit753 <sumit753@gmail.com>
2017-03-28 23:45:00 +0530

    GigForm Data saved To Db

--- GigHub/Controllers/GigController.cs
+++ GigHub/Controllers/GigController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModel;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 namespace GigHub.Controllers
@@ -12,6 +14,7 @@ namespace GigHub.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -19,6 +22,26 @@ namespace GigHub.Controllers
                 Genres = _context.Genre.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel GigViewModel)
+        {
+            var ArtistId = User.Identity.GetUserId();
+            var Artist = _context.Users.Single(u => u.Id == ArtistId);
+            var genre = _context.Genre.Single(m => m.Id == GigViewModel.Genre);
+            var gig = new Gig
+            {
+                Artist = Artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", GigViewModel.Date, GigViewModel.Time)),
+                Venue = GigViewModel.Venue,
+                Genre = genre
+            };
+            _context.Gig.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
