04d8da0277c889aba0b3b42bf119ba9740490273
sumit753 <sumit753@gmail.com>
2017-03-28 22:42:00 +0530

    Overriding Convention

---
+++ GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace GigHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gig> Gig { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
