90ac9d236f278db348c2f72d78c53b844442ebe7
sumit753 <sumit753@gmail.com>
2017-03-30 00:08:50 +0530

    Added "Name" to Signup page

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHub.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcommingGigs = _context.Gig.Include(a => a.Artist)
+                                .Where(a => a.DateTime > DateTime.Now);
+            return View(upcommingGigs);
         }
 
         public ActionResult About()
